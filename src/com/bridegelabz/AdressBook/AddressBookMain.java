58d3148b7a39f2acdfe3d6a066ff4bbc6f9185b7
gayuchowdhary <gayuchowdhary51@gmail.com>
2022-06-27 11:57:19 +0530

    Added code to create contact in adress book

---
+++ src/com/bridegelabz/AdressBook/AddressBookMain.java
@@ -0,0 +1,10 @@
+package com.bridegelabz.AdressBook;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("welcome to address book program");
+        AddressBook addressBook = new AddressBook("Gayu", "Chow","Telengana", "Hyderabad", 500032, 123456,"gayuchowdhary51@gmail.com","dvg");
+        System.out.println(addressBook);
+    }
+}
+
